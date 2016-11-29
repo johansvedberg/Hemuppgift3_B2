dd87c19fc8ecd2030e5a5b7d30fba11359d225f0
johansvedberg <johan@svedberg.biz>
2016-11-29 22:46:44 +0100

    Used apache's math-functions to solve the problem

--- src/main/ThresholdScheme.java
+++ src/main/ThresholdScheme.java
@@ -1,5 +1,23 @@
 package main;
 
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionLagrangeForm;
+
 public class ThresholdScheme {
+
+	
+
+	public ThresholdScheme() {
+
+		
+		double[] x = { 1, 2, 3, 5, 6};
+		double[] y = { 275, 1908, 7677, 50751, 101700 };
+
+		PolynomialFunctionLagrangeForm pub = new PolynomialFunctionLagrangeForm(x, y);
+	
+		double[] values = pub.getCoefficients();
+		
+		System.out.println(values[0]);
+
+	}
 
 }
