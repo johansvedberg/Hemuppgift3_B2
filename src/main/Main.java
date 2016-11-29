75c0f2b9e58433f3ddb82a0648e9558f75a2c7ec
johansvedberg <johan@svedberg.biz>
2016-11-29 23:41:35 +0100

    Much cleaner code, not sure about the inputs

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,15 @@ package main;
 public class Main {
 	public static void main(String args[]) {
 
-		ThresholdScheme thresholdScheme = new ThresholdScheme();
+		double[] polynomial = { 13, 8, 11, 1, 5 };
+
+		double[] participants = { 1, 2, 4, 5, 7 };
+
+		double[] shares = { 0, 75, 75, 54, 52, 77, 54, 43 };
+
+		double[] points = { 0, 2782, 30822, 70960, 256422 };
+
+		ThresholdScheme thresholdScheme = new ThresholdScheme(polynomial, participants, shares, points);
 
 	}
 
--- src/main/ThresholdScheme.java
+++ src/main/ThresholdScheme.java
@@ -1,22 +1,33 @@
 package main;
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionLagrangeForm;
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 
 public class ThresholdScheme {
 
-	
+	public ThresholdScheme(double[] polynomial, double[] participants, double[] shares, double[] points ) {
 
-	public ThresholdScheme() {
+		PolynomialFunction privPoly = new PolynomialFunction(polynomial);
 
+		// double[] shares = { privPoly.value(1), 75, 75, 54, 52, 77, 54, 43 };
+
+		shares[0] = privPoly.value(1);
+
+		double i = 0;
+		for (double d : shares) {
+			i = i + d;
+
+		}
+
+		//double[] y = { i, 2782, 30822, 70960, 256422 };
 		
-		double[] x = { 1, 2, 3, 5, 6};
-		double[] y = { 275, 1908, 7677, 50751, 101700 };
+		points[0] = i;
 
-		PolynomialFunctionLagrangeForm pub = new PolynomialFunctionLagrangeForm(x, y);
-	
+		PolynomialFunctionLagrangeForm pub = new PolynomialFunctionLagrangeForm(participants, points);
+
 		double[] values = pub.getCoefficients();
-		
-		System.out.println(values[0]);
+
+		System.out.println("Deactivation code: " + (int) values[0]);
 
 	}
 
